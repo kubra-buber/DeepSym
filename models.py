243551ae7a803e2d96a47ad05af29f6f8fdf1c2a
kubra-buber <kubrabuberrrr@gmail.com>
2026-04-17 18:15:56 +0300

    refactor: merge optimizers and add joint loss function in models.py

--- models.py
+++ models.py
@@ -12,14 +12,12 @@ class EffectRegressorMLP:
         self.encoder2 = build_encoder(opts, 2).to(self.device)
         self.decoder1 = MLP([opts["code1_dim"] + 3] + [opts["hidden_dim"]] * opts["depth"] + [3]).to(self.device)
         self.decoder2 = MLP([opts["code2_dim"] + opts["code1_dim"]*2] + [opts["hidden_dim"]] * opts["depth"] + [6]).to(self.device)
-        self.optimizer1 = torch.optim.Adam(lr=opts["learning_rate1"],
+        
+        # Unified optimizer for all 4 networks
+        self.optimizer = torch.optim.Adam(lr=opts.get("learning_rate1", 0.0001), 
                                            params=[
                                                {"params": self.encoder1.parameters()},
-                                               {"params": self.decoder1.parameters()}],
-                                           amsgrad=True)
-
-        self.optimizer2 = torch.optim.Adam(lr=opts["learning_rate2"],
-                                           params=[
+                                               {"params": self.decoder1.parameters()},
                                                {"params": self.encoder2.parameters()},
                                                {"params": self.decoder2.parameters()}],
                                            amsgrad=True)
@@ -43,8 +41,9 @@ class EffectRegressorMLP:
         obs = sample["observation"].to(self.device)
         effect = sample["effect"].to(self.device)
 
-        with torch.no_grad():
-            h1 = self.encoder1(obs.reshape(-1, 1, obs.shape[2], obs.shape[3]))
+        # Removed with torch.no_grad():
+        # Gradients from paired task now flow back into encoder1
+        h1 = self.encoder1(obs.reshape(-1, 1, obs.shape[2], obs.shape[3]))
         h1 = h1.reshape(obs.shape[0], -1)
         h2 = self.encoder2(obs)
         h_aug = torch.cat([h1, h2], dim=-1)
@@ -52,69 +51,78 @@ class EffectRegressorMLP:
         loss = self.criterion(effect_pred, effect)
         return loss
 
-    def one_pass_optimize(self, loader, level):
-        running_avg_loss = 0.0
-        for i, sample in enumerate(loader):
-            if level == 1:
-                self.optimizer1.zero_grad()
-                loss = self.loss1(sample)
-                loss.backward()
-                self.optimizer1.step()
-            else:
-                self.optimizer2.zero_grad()
-                loss = self.loss2(sample)
-                loss.backward()
-                self.optimizer2.step()
-            running_avg_loss += loss.item()
-            self.iteration += 1
-        return running_avg_loss/i
+    def loss(self, sample_single, sample_paired):
+        # Calculate individual losses and sum them up
+        l1 = self.loss1(sample_single)
+        l2 = self.loss2(sample_paired)
+        return l1 + l2
 
-    def train(self, epoch, loader, level):
+    # Update arguments to take both loaders
+    def one_pass_optimize(self, loader1, loader2):
+        running_avg_loss = 0.0
+        i = 0
+        # ZIP THEM HERE: This creates a fresh iterator every epoch
+        for sample_single, sample_paired in zip(loader1, loader2):
+            self.optimizer.zero_grad()
+            total_loss = self.loss(sample_single, sample_paired)
+            total_loss.backward()
+            self.optimizer.step()
+            
+            running_avg_loss += total_loss.item()
+            self.iteration += 1
+            i += 1
+            
+        return running_avg_loss / max(1, i)
+
+    # Update arguments to take both loaders
+    def train(self, epoch, loader1, loader2):
         best_loss = 1e100
         for e in range(epoch):
-            epoch_loss = self.one_pass_optimize(loader, level)
+            # Pass both loaders to the optimizer
+            epoch_loss = self.one_pass_optimize(loader1, loader2)
             if epoch_loss < best_loss:
                 best_loss = epoch_loss
-                self.save(self.save_path, "_best", level)
+                self.save(self.save_path, "_best")
             print("Epoch: %d, iter: %d, loss: %.4f" % (e+1, self.iteration, epoch_loss))
-            self.save(self.save_path, "_last", level)
+            self.save(self.save_path, "_last")
 
-    def load(self, path, ext, level):
-        if level == 1:
-            encoder = self.encoder1
-            decoder = self.decoder1
-        else:
-            encoder = self.encoder2
-            decoder = self.decoder2
+    def load(self, path, ext):
+        # Load all components at once
+        enc1_dict = torch.load(os.path.join(path, "encoder1"+ext+".ckpt"))
+        dec1_dict = torch.load(os.path.join(path, "decoder1"+ext+".ckpt"))
+        self.encoder1.load_state_dict(enc1_dict)
+        self.decoder1.load_state_dict(dec1_dict)
+        
+        enc2_dict = torch.load(os.path.join(path, "encoder2"+ext+".ckpt"))
+        dec2_dict = torch.load(os.path.join(path, "decoder2"+ext+".ckpt"))
+        self.encoder2.load_state_dict(enc2_dict)
+        self.decoder2.load_state_dict(dec2_dict)
 
-        encoder_dict = torch.load(os.path.join(path, "encoder"+str(level)+ext+".ckpt"))
-        decoder_dict = torch.load(os.path.join(path, "decoder"+str(level)+ext+".ckpt"))
-        encoder.load_state_dict(encoder_dict)
-        decoder.load_state_dict(decoder_dict)
+    def save(self, path, ext):
+        # Save all components at once using the original naming structure
+        enc1_dict = self.encoder1.eval().cpu().state_dict()
+        dec1_dict = self.decoder1.eval().cpu().state_dict()
+        torch.save(enc1_dict, os.path.join(path, "encoder1"+ext+".ckpt"))
+        torch.save(dec1_dict, os.path.join(path, "decoder1"+ext+".ckpt"))
+        self.encoder1.train().to(self.device)
+        self.decoder1.train().to(self.device)
 
-    def save(self, path, ext, level):
-        if level == 1:
-            encoder = self.encoder1
-            decoder = self.decoder1
-        else:
-            encoder = self.encoder2
-            decoder = self.decoder2
+        enc2_dict = self.encoder2.eval().cpu().state_dict()
+        dec2_dict = self.decoder2.eval().cpu().state_dict()
+        torch.save(enc2_dict, os.path.join(path, "encoder2"+ext+".ckpt"))
+        torch.save(dec2_dict, os.path.join(path, "decoder2"+ext+".ckpt"))
+        self.encoder2.train().to(self.device)
+        self.decoder2.train().to(self.device)
 
-        encoder_dict = encoder.eval().cpu().state_dict()
-        decoder_dict = decoder.eval().cpu().state_dict()
-        torch.save(encoder_dict, os.path.join(path, "encoder"+str(level)+ext+".ckpt"))
-        torch.save(decoder_dict, os.path.join(path, "decoder"+str(level)+ext+".ckpt"))
-        encoder.train().to(self.device)
-        decoder.train().to(self.device)
-
-    def print_model(self, level):
-        encoder = self.encoder1 if level == 1 else self.encoder2
-        decoder = self.decoder1 if level == 1 else self.decoder2
-        print("="*10+"ENCODER"+"="*10)
-        print(encoder)
-        print("parameter count: %d" % utils.get_parameter_count(encoder))
-        print("="*27)
-        print("="*10+"DECODER"+"="*10)
-        print(decoder)
-        print("parameter count: %d" % utils.get_parameter_count(decoder))
+    def print_model(self):
+        print("="*10+"ENCODER 1"+"="*10)
+        print(self.encoder1)
+        print("parameter count: %d" % utils.get_parameter_count(self.encoder1))
+        print("="*10+"DECODER 1"+"="*10)
+        print(self.decoder1)
+        print("="*10+"ENCODER 2"+"="*10)
+        print(self.encoder2)
+        print("parameter count: %d" % utils.get_parameter_count(self.encoder2))
+        print("="*10+"DECODER 2"+"="*10)
+        print(self.decoder2)
         print("="*27)
--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import torch
 from models import EffectRegressorMLP
 import data
 
-parser = argparse.ArgumentParser("Train effect prediction models.")
+parser = argparse.ArgumentParser("Train effect prediction models end-to-end.")
 parser.add_argument("-opts", help="option file", type=str, required=True)
 args = parser.parse_args()
 
@@ -14,6 +14,8 @@ opts = yaml.safe_load(open(args.opts, "r"))
 if not os.path.exists(opts["save"]):
     os.makedirs(opts["save"])
 opts["time"] = time.asctime(time.localtime(time.time()))
+
+# Standard opts.yaml file generation
 file = open(os.path.join(opts["save"], "opts.yaml"), "w")
 yaml.dump(opts, file)
 file.close()
@@ -22,23 +24,25 @@ print(yaml.dump(opts))
 device = torch.device(opts["device"])
 
 # load the first level data
-transform = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
-trainset = data.SingleObjectData(transform=transform)
-loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size1"], shuffle=True)
-
-model = EffectRegressorMLP(opts)
-if opts["load"] is not None:
-    model.load(opts["load"], ext="", level=1)
-    model.load(opts["load"], ext="", level=2)
-model.print_model(1)
-model.train(opts["epoch1"], loader, 1)
-
-# load the best encoder1
-model.load(opts["save"], "_best", 1)
+transform1 = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
+trainset1 = data.SingleObjectData(transform=transform1)
+loader1 = torch.utils.data.DataLoader(trainset1, batch_size=opts["batch_size1"], shuffle=True)
 
 # load the second level data
-transform = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
-trainset = data.PairedObjectData(transform=transform)
-loader = torch.utils.data.DataLoader(trainset, batch_size=opts["batch_size2"], shuffle=True)
-model.print_model(2)
-model.train(opts["epoch2"], loader, 2)
+transform2 = data.default_transform(size=opts["size"], affine=True, mean=0.279, std=0.0094)
+trainset2 = data.PairedObjectData(transform=transform2)
+loader2 = torch.utils.data.DataLoader(trainset2, batch_size=opts.get("batch_size2", opts["batch_size1"]), shuffle=True)
+
+# Initialize Model
+model = EffectRegressorMLP(opts)
+if opts.get("load") is not None:
+    model.load(opts["load"], ext="")
+    
+model.print_model()
+
+# Define total epochs 
+epochs = opts.get("epoch", opts.get("epoch1", 100))
+
+# Pass loader1 and loader2 separately
+print(f"Starting end-to-end training for {epochs} epochs...")
+model.train(epochs, loader1, loader2)
